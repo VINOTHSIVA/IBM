80581094be700e210211e8021dd4eab75617c857
VINOTHSIVA <35728684+VINOTHSIVA@users.noreply.github.com>
2018-02-10 09:28:21 +0530

    Create profit.c

---
+++ profit.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int amount,total,amount1;
+    printf("enter the values of amount");
+    scanf("%d",&amount);
+    printf("enter the values of amount1");
+    scanf("%d",&amount1);
+    total=amount-amount1;
+    printf("%d",total);
+    
+}
