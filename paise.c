bfb92150d8851d92eebb3e73f0e4e4b99aa12d84
VINOTHSIVA <35728684+VINOTHSIVA@users.noreply.github.com>
2018-02-10 10:15:36 +0530

    Create paise.c

---
+++ paise.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    float amount;
+    char total;
+    printf("enter the values of amount");
+    scanf("%f",&amount);
+    total=amount*100;
+    printf("%c",total);
+}
